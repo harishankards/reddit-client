83465eb1182cc7ed3902b98738378413b9d0176d
harishankards <harishankards@gmail.com>
2017-08-01 12:21:09 +0530

    Configure firebase to our project and initialise it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import * as firebase from 'firebase';
+import config  from './firebase-config';
 
 class App extends Component {
+  constructor(){
+    super();
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <div className="App">
---
+++ src/firebase-config.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyAoxRLqfmoOD5zceRgJQyw29LL78mHSIso",
+    authDomain: "reddit-client-98005.firebaseapp.com",
+    databaseURL: "https://reddit-client-98005.firebaseio.com",
+    projectId: "reddit-client-98005",
+    storageBucket: "reddit-client-98005.appspot.com",
+    messagingSenderId: "776071029731"
+}
