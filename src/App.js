f8b885331f381babd71d7c790e2a631999a6dd5d
harishankards <harishankards@gmail.com>
2017-08-01 11:24:47 +0530

    Configure eslint in the project

--- src/App.js
+++ src/App.js
@@ -5,15 +5,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        Hello World
       </div>
     );
   }
 }
-
 export default App;
